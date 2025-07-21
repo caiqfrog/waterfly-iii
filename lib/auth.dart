9a34583dae792311d02a69cc965a60c31c2acd8c
caiqfrog <caiqfrog@163.com>
2025-07-21 23:16:58 +0800

    enablePublicKeyPinningBypassForLocalTrustAnchors=true

--- lib/auth.dart
+++ lib/auth.dart
@@ -94,13 +94,15 @@ class AuthErrorNoInstance extends AuthError {
 
 // http.Client get httpClient =>
 //     CronetClient.fromCronetEngine(CronetEngine.build(), closeEngine: false);
-http.Client get httpClient {
-  final engineBuilder = CronetEngine.Builder();
-  // 启用跳过证书验证
-  engineBuilder.enableInsecureConnections(true);
-  final cronetEngine = engineBuilder.build();
-  return CronetClient.fromCronetEngine(cronetEngine, closeEngine: false);
-}
+// http.Client get httpClient {
+//   final engineBuilder = CronetEngine.Builder();
+//   // 启用跳过证书验证
+//   engineBuilder.enableInsecureConnections(true);
+//   final cronetEngine = engineBuilder.build();
+//   return CronetClient.fromCronetEngine(cronetEngine, closeEngine: false);
+// }
+http.Client get httpClient =>
+     CronetClient.fromCronetEngine(CronetEngine.build(enablePublicKeyPinningBypassForLocalTrustAnchors: true), closeEngine: false);
 
 class APIRequestInterceptor implements Interceptor {
   APIRequestInterceptor(this.headerFunc);
